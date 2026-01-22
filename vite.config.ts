75abf158d34003775db18020346dc67f6e388438
RangSeoAlveria <139760152+RangSeoAlveria@users.noreply.github.com>
2026-01-22 14:50:00 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { metaImagesPlugin } from "./vite-plugin-meta-images";
 
 export default defineConfig({
+  base: "/portofolio-showcase/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
