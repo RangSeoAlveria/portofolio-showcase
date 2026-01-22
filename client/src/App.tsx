14f3a92e75d012a38f893ab41f3543958f3b92cb
RangSeoAlveria <139760152+RangSeoAlveria@users.noreply.github.com>
2026-01-22 15:24:38 +0700

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter, hashLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -20,10 +20,16 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+        <WouterRouter hook={hashLocation}>
+          <Switch>
+            <Route path="/" component={Home} />
+            <Route component={NotFound} />
+          </Switch>
+        </WouterRouter>
       </TooltipProvider>
     </QueryClientProvider>
   );
 }
+
 
 export default App;
